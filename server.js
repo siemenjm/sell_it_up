40ce154e31bd8655dc874bdec7360ff953c88881
Jared Siemen <siemenjm@gmail.com>
2022-08-16 11:30:47 -0400

    Do some restful stuff

--- server.js
+++ server.js
@@ -3,7 +3,26 @@ const app = express();
 const PORT = 4000;
 
 // temp, simulated database
-const products = ['t-shirt', 'shoes', 'necklace'];
+const products = [
+    {
+      name: 'Internet Friends',
+      price: 29,
+      image:
+        'https://cdn.shopify.com/s/files/1/1297/1509/products/hero1_6de889fb-b540-49e4-b733-3af0baaa7f63_x1440.jpg?v=1571274629',
+    },
+    {
+      name: 'Angry Pants',
+      price: 35,
+      image:
+        'https://cdn.shopify.com/s/files/1/1297/1509/products/HERO_c5b0ec76-ad06-4cc7-a165-6129e11a8ff6_x1440.jpg?v=1571274622',
+    },
+    {
+      name: 'Dead Cool',
+      price: 50,
+      image:
+        'https://cdn.shopify.com/s/files/1/1297/1509/products/hero1_40030160-f468-4d50-8f30-c8b9733ce84e_x1440.jpg?v=1575020412',
+    },
+  ];
 
 // routes/controllers
 
@@ -13,6 +32,14 @@ app.get('/products/awesome', (req, res) => {
 
 app.get('/products/:productIndex', (req, res) => {
     res.send(products[req.params.productIndex]);
+});
+
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+
+app.get('/', (req,res) => {
+    res.redirect('/products');
 });
 
 app.get('/dogs/:name/:breed', (req, res) => {
